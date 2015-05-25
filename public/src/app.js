001cf149f50cae158c82568f7a09bb6db21543d9
Joan Miralles <jmirallespaez@gmail.com>
2015-05-25 18:42:15 +0200

    routing

--- public/src/app.js
+++ public/src/app.js
@@ -1,4 +1,10 @@
-angular.module('GamesApp', [])
-    .run(function($rootScope) {
-    $rootScope.message = "Hello Angular!" ;
+angular.module('GamesApp', ['ngRoute'])
+    .config(function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/contacts', {
+                controller: 'LisController',
+                templateUrl: 'views/list.html'
+            });
+        $locationProvider.html5Mode(true);
+        
 });
